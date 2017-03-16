2f6dc9a75abc755a3bf3c9019ce004631901a415
Julio César Paredes <jparedes@marnagarage.com>
2017-03-16 11:30:50 -0400

    Probando composer 3

--- conexion.php
+++ conexion.php
@@ -12,10 +12,10 @@ $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD
 
 $conexion_bd = new mysqli("$hotsbd","$usuariobd","$clavebd","$basededatos")
     or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
-    if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    if ($conexion_bd->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $conexion_bd->connect_errno . ") " . $conexion_bd->connect_error;
     die();
 }
     $db = mysqli_select_db($conexion_bd,"$basededatos")
     or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
-?>
+
