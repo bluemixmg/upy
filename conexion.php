d7eb496d398b3b71540d63ac6f626c057e5ba70c
Julio César Paredes <jparedes@marnagarage.com>
2017-03-16 14:08:54 -0400

    Deshaciendo composer, probando conexión

--- conexion.php
+++ conexion.php
@@ -10,12 +10,12 @@ $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD
 
 // Fin de los parametros a configurar para la conexion de la base de datos 
 
-$conexion_bd = new mysqli("$hotsbd","$usuariobd","$clavebd","$basededatos")
-    or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
-    if ($conexion_bd->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $conexion_bd->connect_errno . ") " . $conexion_bd->connect_error;
-    die();
-}
+//$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
+$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd");
+    //or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
+
+if ($conexion_bd->connect_error) {
+    die("Connection failed: " . $conexion_bd->connect_error);
+} 
     $db = mysqli_select_db($conexion_bd,"$basededatos")
     or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
-
