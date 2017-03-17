a98b55c7cbd9e92dd58fbaf754bab94aa0f7d297
Julio César Paredes <jparedes@marnagarage.com>
2017-03-17 09:29:35 -0400

    Cambio de mysqli_connect por mysql_connect

--- conexion.php
+++ conexion.php
@@ -10,12 +10,7 @@ $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD
 
 // Fin de los parametros a configurar para la conexion de la base de datos 
 
-//$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
-$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd");
-    //or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
-
-if ($conexion_bd->connect_error) {
-    die("Connection failed: " . $conexion_bd->connect_error);
-} 
-    $db = mysqli_select_db($conexion_bd,"$basededatos")
+$conexion_bd = mysql_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
+    or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
+    $db = mysql_select_db($conexion_bd,"$basededatos")
     or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
