5b4128542f545a92fd28dc423390b03fcfac5126
Julio César Paredes <jparedes@marnagarage.com>
2017-03-29 16:07:28 -0400

    Prueba de conexión a base de datos en Ferozo 1

--- conexion.php
+++ conexion.php
@@ -9,15 +9,16 @@
     //or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
 // Parametros a configurar para la conexion de la base de datos 
 
-$hotsbd = "localhost";    // sera el valor de nuestra BD 
+$hotsbd = "ecuador.ferozo.com";    // sera el valor de nuestra BD 
 $basededatos = "ev000477_upy";    // sera el valor de nuestra BD 
 
 $usuariobd = "ev000477_upy";    // sera el valor de nuestra BD 
 $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD 
+$port = "2092";
 
 // Fin de los parametros a configurar para la conexion de la base de datos 
 
-$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
+$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos", $port)
     or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
     $db = mysqli_select_db($conexion_bd,"$basededatos")
     or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
--- services/mobile/conexion.php
+++ services/mobile/conexion.php
@@ -7,15 +7,15 @@
     //$conexion_bd = $dbm->getConnection();
 // Parametros a configurar para la conexion de la base de datos 
 
-$hotsbd = "localhost";    // sera el valor de nuestra BD 
+$hotsbd = "whois.dattatec.com";    // sera el valor de nuestra BD 
 $basededatos = "ev000477_upy";    // sera el valor de nuestra BD 
 
 $usuariobd = "ev000477_upy";    // sera el valor de nuestra BD 
 $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD 
-
+$port = "2092";
 // Fin de los parametros a configurar para la conexion de la base de datos 
 
-$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
+$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos", $port)
     or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
     $db = mysqli_select_db($conexion_bd,"$basededatos")
     or die ("La Base de Datos <b>$basededatos</b> no puede ser localizada");
