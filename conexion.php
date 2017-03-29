d68502988911411113393d61278e4486ae8caf4c
Julio César Paredes <jparedes@marnagarage.com>
2017-03-29 15:28:43 -0400

    Prueba de conexión a base de datos en Ferozo (fuera de Bluemix) 2

--- conexion.php
+++ conexion.php
@@ -17,8 +17,6 @@ $clavebd = "ZAgu22zofu";    // sera el valor de nuestra BD
 
 // Fin de los parametros a configurar para la conexion de la base de datos 
 
-$conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos");
-
 $conexion_bd = mysqli_connect("$hotsbd","$usuariobd","$clavebd","$basededatos")
     or die ("Conexión denegada, el Servidor de Base de datos que solicitas no puede ser localizado"); 
     $db = mysqli_select_db($conexion_bd,"$basededatos")
