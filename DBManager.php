4f62a6174ec9c6205084a8d7dc38b974aa1bc6b5
Julio César Paredes <jparedes@marnagarage.com>
2017-04-03 09:33:28 -0400

    Implementando conexión a servicio de BD Elephant (PostgreSQL)

--- DBManager.php
+++ DBManager.php
@@ -16,18 +16,28 @@ class DBManager {
 
     $services = getenv("VCAP_SERVICES");
     $services_json = json_decode($services,true);
-    $mysql_config = $services_json["cleardb"][0]["credentials"];
+    //$mysql_config = $services_json["cleardb"][0]["credentials"];
+    $pgsql_config = $services_json["elephantsql"][0]["credentials"];
 
-    $db = $mysql_config["name"];
-    $host = $mysql_config["hostname"];
-    $port = $mysql_config["port"];
-    $username = $mysql_config["username"];
-    $password = $mysql_config["password"];
+    //$db = $mysql_config["name"];
+    $db = $pgsql_config["name"];
+    //$host = $mysql_config["hostname"];
+    $host = $pgsql_config["hostname"];
+    //$port = $mysql_config["port"];
+    $port = $pgsql_config["port"];
+    //$username = $mysql_config["username"];
+    $username = $pgsql_config["username"];
+    //$password = $mysql_config["password"];
+    $password = $pgsql_config["password"];
 
-    $conn = mysqli_connect($host, $username, $password, $db, $port);
+    //Solo para pgsql:
+    $dbConnectionString = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $username . " password=" . $password;
+    
+    //$conn = mysqli_connect($host, $username, $password, $db, $port);
+    $conn = pg_connect($dbConnectionString);
   
     if(! $conn ){
-      die('No se puede conectar a BD: ' . mysqli_error($conn));
+      die('No se puede conectar a BD: ' . pg_errormessage($conn));
     }
 
     //mysql_select_db($db);
